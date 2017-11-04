0bf9e9c8612a1945c5781a00387108db2f3d94c0
pukup <urozdiego@gmail.com>
2017-11-04 20:37:26 +0100

    Paquete "tarea03" con enumerado "Color"

---
+++ src/tarea03/Color.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author lol
+ * En ajedrez cada rey puede tener uno de dos colores
+ */
+public enum Color {
+    BLANCO,NEGRO;
+}
