2494246a29f03d8b0c54909e9426a61724b396ed
pukup <urozdiego@gmail.com>
2017-11-04 20:41:57 +0100

    Paquete "tarea03" con enumerado "Direccion"

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author lol
+ * En ajedrez el rey puede mover a cualquiera de las 8 casillas adyacentes.
+ */
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE;
+}
