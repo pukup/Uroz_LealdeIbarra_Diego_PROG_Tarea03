25f91d2963ac7443410237cb7a530c8c6ac4c0ef
pukup <urozdiego@gmail.com>
2017-11-10 20:54:27 +0100

    Clase "Rey" constructor(Color)

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -10,10 +10,21 @@ package tarea03;
  * @author pukup
  */
 public class Rey {
-    
+
     private Color color;
     private Posicion posicion;
-    
-    
-    
+
+
+    public Rey(Color color) {
+        switch (color) {
+            case BLANCO:
+                this.color = Color.BLANCO;
+                this.posicion = new Posicion('e', 1);
+                break;
+            case NEGRO:
+                this.color = Color.NEGRO;
+                this.posicion = new Posicion('e', 8);
+                break;
+        }
+    }
 }
