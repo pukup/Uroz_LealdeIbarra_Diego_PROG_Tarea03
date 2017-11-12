f6e02eda0be059be2316f7341c1a54aec9a7961e
pukup <urozdiego@gmail.com>
2017-11-12 12:24:50 +0100

    Método "main" + menú.

--- src/tarea03/Principal.java
+++ src/tarea03/Principal.java
@@ -4,16 +4,84 @@
  * and open the template in the editor.
  */
 package tarea03;
+
+import utilidades.Entrada;
+
 /**
  *
  * @author pukup
  */
 public class Principal {
-    
-    
-        public static void main(String[] args) {
 
-            
+    public static void main(String[] args) {
+
+        Rey rey;
+        int num_color;
+        int num_menu;
+        int num_direccion;
+
+        do {
+            System.out.printf("Introduzca un número: %n 1-Para seleccionar rey negro %n 2-Para seleccionar rey blanco %n");
+            num_color = Entrada.entero();
+        } while (num_color < 1 || num_color > 2);
+
+        if (num_color == 1) {
+            rey = new Rey(Color.NEGRO);
+        } else {
+            rey = new Rey(Color.BLANCO);
         }
-       
+
+        Posicion posicion = rey.getPosicion();
+
+        do {
+            int posicion_fila = posicion.getFila();
+            int posicion_columna = posicion.getColumna();
+            System.out.printf("Introduzca un número: %n 1-Para desplazar el rey. %n 2-Mostrar la información del rey. %n 3-Salir del programa. %n");
+            num_menu = Entrada.entero();
+            switch (num_menu) {
+                case 1:
+                    do {
+                        System.out.printf("Introduzca un número: %n 1-%7$s %4$s%2$s %n 2-%7$s %4$s%3$s%n 3-%7$s %5$s%1$s%n 4-%7$s %6$s%1$s%n 5-%7$s %5$s%2$s%n 6-%7$s %5$s%3$s%n 7-%7$s %6$s%2$s%n 8-%7$s %6$s%3$s%n", posicion_fila, posicion_fila + 1, posicion_fila - 1, ((char) (posicion_columna)), ((char) (posicion_columna + 1)), ((char) (posicion_columna - 1)), "Para mover a la casilla");
+                        num_direccion = Entrada.entero();
+                    } while (num_direccion < 1 || num_direccion > 8);
+                    switch (num_direccion) {
+                        case 1:
+                            rey.Mueve(Direccion.NORTE);
+                            break;
+                        case 2:
+                            rey.Mueve(Direccion.SUR);
+                            break;
+                        case 3:
+                            rey.Mueve(Direccion.ESTE);
+                            break;
+                        case 4:
+                            rey.Mueve(Direccion.OESTE);
+                            break;
+                        case 5:
+                            rey.Mueve(Direccion.NORESTE);
+                            break;
+                        case 6:
+                            rey.Mueve(Direccion.SURESTE);
+                            break;
+                        case 7:
+                            rey.Mueve(Direccion.NOROESTE);
+                            break;
+                        case 8:
+                            rey.Mueve(Direccion.SUROESTE);
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Rey " + rey.getColor() + ": " + rey.getPosicion());
+                    System.out.println("");
+                    break;
+
+                case 3:
+                    System.out.println("Fin del programa.");
+                    break;
+            }
+
+        } while (num_menu != 3);
+    }
 }
--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -6,12 +6,13 @@
 package tarea03;
 
 /**
- * Esta clase define color y posición de un rey mediante dos atributos.
- * Un atributo tipo enumerado "Color" y una instancia "posición" de la clase "Posicion".
- * Dispone de metodos "getter", "toString" y el metodo "Mueve" que actualiza la posición de las instancias creadas.
+ * Esta clase define color y posición de un rey mediante dos atributos. Un
+ * atributo tipo enumerado "Color" y una instancia "posición" de la clase
+ * "Posicion". Dispone de metodos "getter", "toString" y el metodo "Mueve" que
+ * actualiza la posición de las instancias creadas.
  */
 public class Rey {
-    
+
 //Atributos que determinan color y posición.
     private Color color;
     private Posicion posicion;
@@ -21,7 +22,7 @@ public class Rey {
         this.color = Color.BLANCO;
         this.posicion = new Posicion('e', 1);
     }
-    
+
 //Constructor para definir el color con una posición asociada por defecto.
     public Rey(Color color) {
         switch (color) {
@@ -36,7 +37,6 @@ public class Rey {
         }
     }
 
-    
 //Metodos getter.    
     public Color getColor() {
         return color;
@@ -46,68 +46,69 @@ public class Rey {
         return posicion;
     }
 
-    
 //Metodo que convierte a String los atributos.    
     public String toString() {
         return color + ": " + posicion;
     }
 
-    
 //Si es posible actualiza la posición de un objeto en base a las direcciones del enumerdao "Dirección".
 //En caso contrario sigue los parámetros fijados por la clase "Posición".
     public void Mueve(Direccion direccion) {
 
         int fila = posicion.getFila();
         int numero_columna = posicion.getColumna();
-        
+
         switch (direccion) {
             case NORTE:
-                if(Ilegal(Direccion.NORTE)){
-                    Ilegal(Direccion.NORTE);
-                }
-                else{
+                if (Ilegal(Direccion.NORTE)) {
                     posicion.setFila(fila + 1);
-                }                            
+                }
                 break;
             case SUR:
-                Ilegal(Direccion.SUR);
-                posicion.setFila(fila - 1);
+                if (Ilegal(Direccion.SUR)) {
+                    posicion.setFila(fila - 1);
+                }
                 break;
             case ESTE:
-                Ilegal(Direccion.ESTE);
-                posicion.setColumna((char) (numero_columna + 1));
+                if (Ilegal(Direccion.ESTE)) {
+                    posicion.setColumna((char) (numero_columna + 1));
+                }
                 break;
             case OESTE:
-                Ilegal(Direccion.OESTE);
-                posicion.setColumna((char) (numero_columna - 1));
+                if (Ilegal(Direccion.OESTE)) {
+                    posicion.setColumna((char) (numero_columna - 1));
+                }
                 break;
             case NORESTE:
-                Ilegal(Direccion.NORESTE);
-                posicion.setFila(fila + 1);
-                posicion.setColumna((char) (numero_columna + 1));
+                if (Ilegal(Direccion.NORESTE)) {
+                    posicion.setFila(fila + 1);
+                    posicion.setColumna((char) (numero_columna + 1));
+                }
                 break;
             case SURESTE:
-                Ilegal(Direccion.SURESTE);
-                posicion.setFila(fila - 1);
-                posicion.setColumna((char) (numero_columna + 1));
+                if (Ilegal(Direccion.SURESTE)) {
+                    posicion.setFila(fila - 1);
+                    posicion.setColumna((char) (numero_columna + 1));
+                }
                 break;
             case NOROESTE:
-                Ilegal(Direccion.NOROESTE);
-                posicion.setFila(fila + 1);
-                posicion.setColumna((char) (numero_columna - 1));
+                if (Ilegal(Direccion.NOROESTE)) {
+                    posicion.setFila(fila + 1);
+                    posicion.setColumna((char) (numero_columna - 1));
+                }
                 break;
             case SUROESTE:
-                Ilegal(Direccion.SUROESTE);
-                posicion.setFila(fila - 1);
-                posicion.setColumna((char) (numero_columna - 1));
+                if (Ilegal(Direccion.SUROESTE)) {
+                    posicion.setFila(fila - 1);
+                    posicion.setColumna((char) (numero_columna - 1));
+                }
                 break;
         }
-
     }
 
 //Informa cuando la posición está fuera del marco bidimensional fijado.   
     private boolean Ilegal(Direccion direccion) {
-        
+
         int fila = posicion.getFila();
         int columna = posicion.getColumna();
         boolean ilegal = true;
@@ -116,26 +117,30 @@ public class Rey {
                 if (fila + 1 < 1 || fila + 1 > 8) {
                     System.out.print("Movimiento ilegal.");
                     ilegal = false;
-                }              
+                }
                 break;
+
             case SUR:
                 if (fila - 1 < 1 || fila - 1 > 8) {
                     System.out.print("Movimiento ilegal.");
                     ilegal = false;
                 }
                 break;
+
             case ESTE:
                 if (columna + 1 < 'a' || columna + 1 > 'h') {
                     System.out.print("Movimiento ilegal.");
                     ilegal = false;
                 }
                 break;
+
             case OESTE:
                 if (columna - 1 < 'a' || columna - 1 > 'h') {
                     System.out.print("Movimiento ilegal.");
                     ilegal = false;
                 }
                 break;
+
             case NORESTE:
                 if (fila + 1 < 1 || fila + 1 > 8) {
                     System.out.print("Movimiento ilegal.");
@@ -146,6 +151,7 @@ public class Rey {
                     ilegal = false;
                 }
                 break;
+
             case SURESTE:
                 if (fila - 1 < 1 || fila - 1 > 8) {
                     System.out.print("Movimiento ilegal por ");
@@ -156,6 +162,7 @@ public class Rey {
                     ilegal = false;
                 }
                 break;
+
             case NOROESTE:
                 if (fila + 1 < 1 || fila + 1 > 8) {
                     System.out.print("Movimiento ilegal por ");
@@ -166,6 +173,7 @@ public class Rey {
                     ilegal = false;
                 }
                 break;
+
             case SUROESTE:
                 if (fila - 1 < 1 || fila - 1 > 8) {
                     System.out.print("Movimiento ilegal por ");
